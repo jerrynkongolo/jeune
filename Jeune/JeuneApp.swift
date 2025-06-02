2983274b5bb281c06bf9731250991f7b19fdbcea
Jerry Munyoka Nkongolo <jerry@tiye.services>
2025-06-02 23:45:32 +0300

    Initial Commit

---
+++ Jeune/JeuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeuneApp.swift
+//  Jeune
+//
+//  Created by Jerry Nkongolo on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct JeuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JeuneTests/JeuneTests.swift
@@ -0,0 +1,17 @@
+//
+//  JeuneTests.swift
+//  JeuneTests
+//
+//  Created by Jerry Nkongolo on 02/06/2025.
+//
+
+import Testing
+@testable import Jeune
+
+struct JeuneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
